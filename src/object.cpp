6e5654318757c3b8c1673612d50e8f372d4bb397
gammelalf <31348226+gammelalf@users.noreply.github.com>
2022-07-25 02:51:24 +0200

    Implemented all read only methods for OSMObject

--- src/object.cpp
+++ src/object.cpp
@@ -1,8 +1,16 @@
 #include "osmium/osm/tag.hpp"
 #include "osmium/osm/object.hpp"
 
+#define OSMObject(return_type, method_name) return_type OSMObject_##method_name(const osmium::OSMObject &object) { return object.method_name(); }
 extern "C" {
-    const osmium::TagList &object_tags(const osmium::OSMObject &object) {
-        return object.tags();
-    }
+    OSMObject(osmium::object_id_type, id)
+    OSMObject(osmium::unsigned_object_id_type, positive_id)
+    OSMObject(bool, deleted)
+    OSMObject(bool, visible)
+    OSMObject(osmium::object_version_type, version)
+    OSMObject(osmium::user_id_type, uid)
+    OSMObject(bool, user_is_anonymous)
+    OSMObject(osmium::Timestamp, timestamp)
+    OSMObject(const char *, user)
+    OSMObject(const osmium::TagList &, tags)
 }
--- src/object.rs
+++ src/object.rs
@@ -1,16 +1,100 @@
+use std::ffi::CStr;
+use std::os::raw::c_char;
 use std::ops::{Deref, DerefMut};
 use crate::tag_list::TagList;
 
+/// Base class for OSM 's objects:
+/// - [`Node`]
+/// - [`Way`]
+/// - Relation
+/// - [`Area`]
+///
+/// [`Node`]: crate::node::Node
+/// [`Way`]: crate::way::Way
+/// [`Area`]: crate::area::Area
 pub enum OSMObject {}
 
 impl OSMObject {
+    /// Get ID of this object.
+    pub fn id(&self) -> ObjectId {
+        unsafe { OSMObject_id(self) }
+    }
+
+    /// Get absolute value of the ID of this object.
+    pub fn positive_id(&self) -> UnsignedObjectId {
+        unsafe { OSMObject_positive_id(self) }
+    }
+
+    /// Is this object marked as deleted?
+    pub fn deleted(&self) -> bool {
+        unsafe { OSMObject_deleted(self) }
+    }
+
+    /// Is this object marked visible (ie not deleted)?
+    pub fn visible(&self) -> bool {
+        unsafe { OSMObject_visible(self) }
+    }
+
+    /// Get version of this object.
+    pub fn version(&self) -> ObjectVersion {
+        unsafe { OSMObject_version(self) }
+    }
+
+    /// Get user id of this object.
+    pub fn uid(&self) -> UserId {
+        unsafe { OSMObject_uid(self) }
+    }
+
+    /// Is this user anonymous?
+    pub fn user_is_anonymous(&self) -> bool {
+        unsafe { OSMObject_user_is_anonymous(self) }
+    }
+
+    /// Get timestamp when this object last changed.
+    pub fn timestamp(&self) -> Timestamp {
+        unsafe { OSMObject_timestamp(self) }
+    }
+
+    /// Get user name for this object.
+    pub fn user(&self) -> &CStr {
+        unsafe { CStr::from_ptr(OSMObject_user(self)) }
+    }
+
+    /// Get the list of tags for this object.
     pub fn tags(&self) -> &TagList {
-        unsafe { object_tags(self) }
+        unsafe { OSMObject_tags(self) }
     }
 }
 
+/// Type for OSM user IDs.
+type UserId = u32;
+
+/// Type for OSM object (node, way, or relation) IDs.
+type ObjectId = i64;
+
+/// Type for OSM object (node, way, or relation) IDs where we only allow positive IDs.
+type UnsignedObjectId = u64;
+
+/// Type for OSM object version number.
+type ObjectVersion = u32;
+
+/// A timestamp. Internal representation is an unsigned 32bit integer holding seconds
+/// since epoch (1970-01-01T00:00:00Z), so this will overflow in 2106.
+/// We can use an unsigned integer here, because the OpenStreetMap project was started
+/// long after 1970, so there will never be dates before that.
+type Timestamp = u32;
+
 extern "C" {
-    fn object_tags(object: &OSMObject) -> &TagList;
+    fn OSMObject_id(object: &OSMObject) -> ObjectId;
+    fn OSMObject_positive_id(object: &OSMObject) -> UnsignedObjectId;
+    fn OSMObject_deleted(object: &OSMObject) -> bool;
+    fn OSMObject_visible(object: &OSMObject) -> bool;
+    fn OSMObject_version(object: &OSMObject) -> ObjectVersion;
+    fn OSMObject_uid(object: &OSMObject) -> UserId;
+    fn OSMObject_user_is_anonymous(object: &OSMObject) -> bool;
+    fn OSMObject_timestamp(object: &OSMObject) -> Timestamp;
+    fn OSMObject_user(object: &OSMObject) -> *const c_char;
+    fn OSMObject_tags(object: &OSMObject) -> &TagList;
 }
 
 macro_rules! impl_subclass {
