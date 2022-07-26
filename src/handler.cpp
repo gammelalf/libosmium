13fd5f9078d157b6590ed0a4c35c7f112ba107ab
gammelalf <31348226+gammelalf@users.noreply.github.com>
2022-07-26 03:15:54 +0200

    Restructured test function into 3 apply functions

--- src/handler.cpp
+++ src/handler.cpp
@@ -1,6 +1,10 @@
 #include "osmium/handler.hpp"      // Handler and all object types
+#include "osmium/handler/node_locations_for_ways.hpp"
+#include "osmium/index/map/flex_mem.hpp"
 #include "osmium/io/pbf_input.hpp" // osmium::io::Reader for pbf files
 #include "osmium/visitor.hpp"      // osmium::apply
+#include "osmium/area/assembler.hpp"
+#include "osmium/area/multipolygon_manager.hpp"
 
 template <class T>
 using HandlerFunc = void (void *, const T &);
@@ -38,10 +42,59 @@ public:
     void flush() { flush_handler(self_pointer); }
 };
 
+using way_creator_map = osmium::index::map::FlexMem<osmium::unsigned_object_id_type, osmium::Location>;
+using way_creator_type = osmium::handler::NodeLocationsForWays<way_creator_map>;
+using area_creator_type = osmium::area::MultipolygonManager<osmium::area::Assembler>;
+
 extern "C" {
-    void test(RustHandler handler, char *file) {
+    void apply(RustHandler handler, char *path) {
+        osmium::io::File file{path};
+
         osmium::io::Reader reader{file};
-        osmium::apply(reader, handler);
+        osmium::apply(
+            reader,
+            handler
+        );
+        reader.close();
+    }
+
+    void apply_with_ways(RustHandler handler, char *path) {
+        osmium::io::File file{path};
+
+        way_creator_map map;
+        way_creator_type way_creator{map};
+        way_creator.ignore_errors();
+
+        osmium::io::Reader reader{file};
+        osmium::apply(
+            reader,
+            way_creator,
+            handler
+        );
+        reader.close();
+    }
+
+    void apply_with_areas(RustHandler handler, char *path, osmium::area::AssemblerConfig config) {
+        const osmium::io::File file{path};
+
+        way_creator_map map;
+        way_creator_type way_creator{map};
+        way_creator.ignore_errors();
+
+        area_creator_type area_creator{config};
+        osmium::relations::read_relations(file, area_creator);
+
+        osmium::io::Reader reader{file, osmium::io::read_meta::no};
+        osmium::apply(
+            reader,
+            way_creator,
+            handler,
+            area_creator.handler(
+                [&handler](const osmium::memory::Buffer &area_buffer) {
+                    osmium::apply(area_buffer, handler);
+                }
+            )
+        );
         reader.close();
     }
 }
--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,7 @@
 use std::ffi::c_void;
 use std::marker::PhantomData;
-use std::os::raw::c_char;
+use std::os::raw::{c_char, c_int};
+use std::ptr;
 
 use crate::area::Area;
 use crate::node_ref_list::{InnerRing, OuterRing, WayNodeList};
@@ -10,7 +11,9 @@ use crate::tag_list::TagList;
 use crate::way::Way;
 
 extern "C" {
-    pub fn test(handler: HandlerTable, file: *const c_char);
+    pub fn apply(handler: HandlerTable, file: *const c_char);
+    pub fn apply_with_ways(handler: HandlerTable, file: *const c_char);
+    pub fn apply_with_areas(handler: HandlerTable, file: *const c_char, config: AreaAssemblerConfig);
 }
 
 pub enum Changeset {}
@@ -77,4 +80,34 @@ pub struct HandlerTable<'a> {
     pub way: *const (),
     pub way_node_list: *const (),
     pub flush: *const (),
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct AreaAssemblerConfig {
+    pub problem_reporter: *const c_void,
+    pub debug_level: c_int,
+    pub check_roles: bool,
+    pub create_empty_areas: bool,
+    pub create_new_style_polygons: bool,
+    pub create_old_style_polygons: bool,
+    pub create_way_polygons: bool,
+    pub keep_type_tag: bool,
+    pub ignore_invalid_locations: bool,
+}
+
+impl Default for AreaAssemblerConfig {
+    fn default() -> Self {
+        AreaAssemblerConfig {
+            problem_reporter: ptr::null(),
+            debug_level: 0,
+            check_roles: false,
+            create_empty_areas: true,
+            create_new_style_polygons: true,
+            create_old_style_polygons: true,
+            create_way_polygons: true,
+            keep_type_tag: false,
+            ignore_invalid_locations: false
+        }
+    }
 }
