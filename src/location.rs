b157eb6bef866ead4ee78324da18dbe19bbe071f
gammelalf <31348226+gammelalf@users.noreply.github.com>
2022-07-31 22:22:08 +0200

    Made underlying Location data public

--- src/location.rs
+++ src/location.rs
@@ -1,7 +1,7 @@
 use std::fmt::{Display, Formatter};
 
 const UNDEFINED_COORDINATE: i32 = i32::MAX;
-const PRECISION: i32 = 10000000;
+pub const PRECISION: i32 = 10000000;
 
 /// A gps coordinate in angles of latitude and longitude
 ///
@@ -11,42 +11,53 @@ const PRECISION: i32 = 10000000;
 #[repr(C)]
 #[derive(Copy, Clone, Debug)]
 pub struct Location {
-    pub(crate) x: i32,
-    pub(crate) y: i32,
+    pub raw_x: i32,
+    pub raw_y: i32,
 }
 
 impl Location {
     pub fn is_defined(&self) -> bool {
-        self.x != UNDEFINED_COORDINATE || self.y != UNDEFINED_COORDINATE
+        self.raw_x != UNDEFINED_COORDINATE || self.raw_y != UNDEFINED_COORDINATE
     }
 
     pub fn is_undefined(&self) -> bool {
-        self.x == UNDEFINED_COORDINATE && self.y == UNDEFINED_COORDINATE
+        self.raw_x == UNDEFINED_COORDINATE && self.raw_y == UNDEFINED_COORDINATE
     }
 
     pub fn is_valid(&self) -> bool {
-        self.x >= -180 * PRECISION
-            && self.x <= 180 * PRECISION
-            && self.y >= -90 * PRECISION
-            && self.y <= 90 * PRECISION
+        self.raw_x >= -180 * PRECISION
+            && self.raw_x <= 180 * PRECISION
+            && self.raw_y >= -90 * PRECISION
+            && self.raw_y <= 90 * PRECISION
     }
 
     pub fn lon(&self) -> f64 {
-        self.x as f64 / PRECISION as f64
+        self.raw_x as f64 / PRECISION as f64
     }
 
     pub fn lat(&self) -> f64 {
-        self.y as f64 / PRECISION as f64
+        self.raw_y as f64 / PRECISION as f64
     }
 }
 
 impl Display for Location {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let (mut x1, mut x2) = (self.x / PRECISION, self.x % PRECISION);
-        let (mut y1, mut y2) = (self.y / PRECISION, self.y % PRECISION);
-        x2 = x2.abs(); y2 = y2.abs();
-        let lat = if self.y > 0 { "N" } else { y1 = -y1; "S" };
-        let lon = if self.x > 0 { "E" } else { x1 = -x1; "W" };
+        let (mut x1, mut x2) = (self.raw_x / PRECISION, self.raw_x % PRECISION);
+        let (mut y1, mut y2) = (self.raw_y / PRECISION, self.raw_y % PRECISION);
+        x2 = x2.abs();
+        y2 = y2.abs();
+        let lat = if self.raw_y > 0 {
+            "N"
+        } else {
+            y1 = -y1;
+            "S"
+        };
+        let lon = if self.raw_x > 0 {
+            "E"
+        } else {
+            x1 = -x1;
+            "W"
+        };
         write!(f, "{}.{}° {} {}.{}° {}", y1, y2, lat, x1, x2, lon)
     }
 }
