3df5fc010f7cbb3c1c40fca725f077f269cc5b67
gammelalf <31348226+gammelalf@users.noreply.github.com>
2022-08-12 23:16:00 +0200

    Implemented an owned version of TagList

--- src/tag_list.rs
+++ src/tag_list.rs
@@ -5,6 +5,7 @@ use std::os::raw::c_char;
 pub enum TagList {}
 
 impl TagList {
+    /// Lookup a key's value
     pub fn get<'a>(&'a self, key: &'_ str) -> Option<&'a str> {
         for (k, v) in self.into_iter() {
             if k == key {
@@ -12,6 +13,11 @@ impl TagList {
             }
         }
         return None;
+    }
+
+    /// Are there any tags at all?
+    pub fn is_empty(&self) -> bool {
+        unsafe { tag_list_begin(self) == tag_list_end(self) }
     }
 }
 impl<'a> IntoIterator for &'a TagList {
@@ -72,4 +78,100 @@ unsafe fn after_null(mut ptr: *const c_char) -> *const c_char {
 extern "C" {
     fn tag_list_begin(list: &TagList) -> *const c_char;
     fn tag_list_end(list: &TagList) -> *const c_char;
+}
+
+/// An owned version of the [`TagList`], implemented purely in rust.
+///
+/// The actual data is stored in the same way as in [`TagList`]
+/// and therefore uses the same accessing logic. But it manages its own memory using a [`Vec<u8>`]
+#[derive(Clone)]
+pub struct OwnedTagList(pub Vec<u8>);
+
+impl OwnedTagList {
+    /// Constructs a new, empty `OwnedTagList`.
+    ///
+    /// The list will not allocate until elements are pushed onto it. (It just calls [`Vec::new`])
+    pub const fn new() -> OwnedTagList {
+        OwnedTagList(Vec::new())
+    }
+
+    /// Internal helper method to push a single string and a trailing `NUL` character
+    ///
+    /// See [`push_pair`] for panic explanation
+    ///
+    /// [`push_pair`]: OwnedTagList::push_pair
+    fn push_str(&mut self, string: &str) {
+        assert!(!string.as_bytes().contains(&0));
+        self.0.extend_from_slice(string.as_bytes());
+        self.0.push(0);
+    }
+
+    /// Add a new key-value pair to the list
+    ///
+    /// This method does not check duplicate keys. It simply pushes the new pair.
+    ///
+    /// # Panics
+    ///
+    /// Panics if any of the arguments contains a `NUL` character.
+    /// This could have been handled by returning an error, but for now I chose not to.
+    pub fn push_pair(&mut self, key: &str, value: &str) {
+        self.push_str(key);
+        self.push_str(value);
+    }
+}
+
+impl From<&TagList> for OwnedTagList {
+    /// Copy a TagList's memory into an owned `Vec`
+    fn from(tag_list: &TagList) -> Self {
+        let bytes = unsafe {
+            let begin = tag_list_begin(tag_list) as *const u8;
+            let end = tag_list_end(tag_list);
+            std::slice::from_raw_parts(begin, end as usize - begin as usize)
+        };
+        let mut tag_list = OwnedTagList::new();
+        tag_list.0.extend_from_slice(bytes);
+        tag_list
+    }
+}
+
+impl<'a> IntoIterator for &'a OwnedTagList {
+    type Item = (&'a str, &'a str);
+    type IntoIter = TagIterator<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let begin = self.0.as_ptr();
+        let len = self.0.len();
+        TagIterator {
+            current: begin as *const c_char,
+            end: (begin as usize + len) as *const c_char,
+            list_lifetime: PhantomData,
+        }
+    }
+}
+
+/// Nice syntax for creating an [`OwnedTagList`] using literals.
+///
+/// Mainly useful to test which would work on [`TagList`] with hardcoded data.
+///
+/// ```
+/// use libosmium::tag_list;
+///
+/// let tags = tag_list! {
+///     "amenity": "bar",
+///     "name": "Hacker's pub",
+///     "opening_hours": "Mo-Su 00:00-06:00,14:00-24:00"
+/// };
+/// ```
+#[macro_export]
+macro_rules! tag_list {
+    ($($key:literal: $value:literal),* $(,)?) => {
+        {
+            let string = concat!(
+                $($key, "\x00", $value, "\x00"),*
+            );
+            let mut bytes = Vec::new();
+            bytes.extend_from_slice(string.as_bytes());
+            $crate::tag_list::OwnedTagList(bytes)
+        }
+    };
 }
